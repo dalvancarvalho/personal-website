3838bfa27dcc520c905066d62e9501fa2f4a1268
Dalvan Carvalho <dalvan.bcarvalho@gmail.com>
2023-10-09 17:16:24 -0300

    refactor: header components and styling

---
+++ src/layout/Header/components/Divider.jsx
@@ -0,0 +1,9 @@
+/* Divider.jsx */
+
+function Divider() {
+  // Vertical divider
+
+  return <span className="h-[2em] w-px bg-slate-200 dark:bg-dark-5 color-transition" />
+}
+
+export default Divider
--- src/layout/Header/index.jsx
+++ src/layout/Header/index.jsx
@@ -9,6 +9,7 @@ import useHeaderAnimation from './animations/useHeaderAnimation'
 import Container from '../Container'
 import SkipToContent from '../../components/Button/SkipToContent'
 import LanguageSwitch from './components/LanguageSwitch'
+import Divider from './components/Divider'
 import Logo from './components/Logo'
 import Menu from './components/Menu'
 import Navbar from './components/Navbar'
@@ -26,11 +27,7 @@ function Header() {
 
   return (
     <header
-      className={`${
-        y === 0 || isMenuOpen
-          ? 'h-20 md:h-24 2xl:h-28 bg-transparent border-b-transparent'
-          : 'h-12 2xl:h-16 bg-slate-50 dark:bg-dark-1 backdrop-blur-[8px] border-b-slate-200 dark:border-b-dark-5' // should stay in one line :(
-      }
+      className={`${y === 0 || isMenuOpen ? 'header-full' : 'header-collapsed'}
       fixed top-0 z-50 w-full 2xl:text-lg bg-opacity-[85%] dark:bg-opacity-[85%]
       border-b transition-[height,background,border] duration-300`}
       ref={refs.scope}
@@ -46,9 +43,7 @@ function Header() {
             // 💻 on medium/large screens, renders the items directly in the header
             <div className="flex items-center gap-6 2xl:gap-8">
               <Navbar t={t} />
-              {pathname !== '/404' && (
-                <span className="h-[2em] w-px bg-slate-200 dark:bg-dark-5 color-transition" />
-              )}
+              {pathname !== '/404' && <Divider />}
               <LanguageSwitch t={t} />
               <ThemeSwitch t={t} />
             </div>
