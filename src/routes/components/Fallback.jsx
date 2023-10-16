94a7aa5714865d8dc2277b3cb12b1c5918473883
Dalvan Carvalho <dalvan.bcarvalho@gmail.com>
2023-10-16 10:48:22 -0300

    update: description

--- src/routes/components/Fallback.jsx
+++ src/routes/components/Fallback.jsx
@@ -4,6 +4,8 @@ import { Suspense } from 'react'
 import Loading from '../../pages/Loading'
 
 function Fallback(props) {
+  // Fallback component that displays a loading page while the selected route loads
+
   const { page } = props
 
   return <Suspense fallback={<Loading />}>{page}</Suspense>
