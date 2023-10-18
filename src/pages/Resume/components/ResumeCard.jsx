6657822a4b3a29d61b3ef62cec3a1682749024eb
Dalvan Carvalho <dalvan.bcarvalho@gmail.com>
2023-10-18 11:11:07 -0300

    update: card size and title

--- src/pages/Resume/components/ResumeCard.jsx
+++ src/pages/Resume/components/ResumeCard.jsx
@@ -12,7 +12,7 @@ function ResumeCard(props) {
 
   return (
     <div
-      className="relative max-w-max pt-4 pb-6 px-6 sm:pt-6 sm:pb-10 sm:px-10 rounded-xl
+      className="relative max-w-max pb-5 px-5 md:pb-6 md:px-6 2xl:pb-7 2xl:px-7 rounded-xl
       bg-slate-100 dark:bg-dark-4 flex flex-col items-center shadow-md color-transition
       group resume-hover"
     >
@@ -23,19 +23,19 @@ function ResumeCard(props) {
         tabIndex="0"
         target="_blank"
       ></a>
-      <div className="mb-4 sm:mb-6 flex items-center gap-4">
+      <div className="my-3 md:my-4 2xl:my-5 flex items-center gap-3 md:gap-4">
         <Paragraph fontWeight="font-bold" textColor="text-slate-800 dark:text-gray-200">
           {title}
         </Paragraph>
         <span
-          className="px-2 py-1 text-xs font-semibold bg-red-600 text-white
-          rounded-md select-none"
+          className="px-[0.7em] py-[0.35em] text-[0.65rem] md:text-xs font-semibold
+          bg-red-600 text-white rounded-md select-none"
         >
           PDF
         </span>
       </div>
       <div className="relative rounded-md overflow-hidden">
-        <img className="w-[350px] 2xl:w-[400px]" alt={altText} src={thumbnail} />
+        <img className="w-72 md:w-[19rem] 2xl:w-80" alt={altText} src={thumbnail} />
         <a download={fileName} href={file} rel="noopener noreferrer" tabIndex="-1">
           <CallToAction
             className="!absolute z-20 bottom-4 left-1/2 -translate-x-1/2 mt-6
