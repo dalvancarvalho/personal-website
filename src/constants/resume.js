c287e0f1652d7a40221ff7ac7e53ceb67f97ffe3
Dalvan Carvalho <dalvan.bcarvalho@gmail.com>
2023-11-21 14:55:03 -0300

    update: resume

--- src/constants/resume.js
+++ src/constants/resume.js
@@ -1,26 +1,27 @@
 /* resume.js */
 
-import cv from '../assets/cv.png' // vai mudar pra 2 arquivos webp
-import resumePt from '../assets/pdf/curriculo.pdf'
-import resumeEn from '../assets/pdf/resume.pdf'
+import resumeEn from '../assets/pdf/resume-en.pdf'
+import resumePt from '../assets/pdf/resume-pt.pdf'
+import thumbEn from '../assets/webp/resume-en.webp'
+import thumbPt from '../assets/webp/resume-pt.webp'
 
 const resume = [
-  // List of resume versions (Brazilian portuguese and English)
+  // List of resume versions (Brazilian Portuguese and English)
 
   {
     altText: 'imagem do currículo de Dalvan Carvalho',
     file: resumePt,
-    fileName: 'Currículo - Dalvan Carvalho',
+    fileName: 'Currículo — Dalvan Carvalho',
     href: '/curriculo.pdf',
-    thumbnail: cv,
+    thumbnail: thumbPt,
     title: 'pages.resume.download.portuguese',
   },
   {
     altText: `image of Dalvan Carvalho's resume`,
     file: resumeEn,
-    fileName: 'Resume - Dalvan Carvalho',
+    fileName: 'Resume — Dalvan Carvalho',
     href: '/resume.pdf',
-    thumbnail: cv,
+    thumbnail: thumbEn,
     title: 'pages.resume.download.english',
   },
 ]
