290f07f647c86adb8a9affa37e61625e260723ae
Dalvan Carvalho <dalvan.bcarvalho@gmail.com>
2024-03-13 20:05:51 -0300

    refactor: keys

--- src/constants/logoText.js
+++ src/constants/logoText.js
@@ -1,19 +1,19 @@
 /* logoText.js */
 
 const ptBR = {
-  ['/']: 'dalvan c',
-  ['/br-geo']: 'br geo',
-  ['/personal-website']: 'website pessoal',
-  ['/resume']: 'currículo',
-  ['/split-it']: 'split it!',
+  '/': 'dalvan c',
+  '/br-geo': 'br geo',
+  '/personal-website': 'website pessoal',
+  '/resume': 'currículo',
+  '/split-it': 'split it!',
 }
 
 const enUS = {
-  ['/']: 'dalvan c',
-  ['/br-geo']: 'br geo',
-  ['/personal-website']: 'personal website',
-  ['/resume']: 'resume',
-  ['/split-it']: 'split it!',
+  '/': 'dalvan c',
+  '/br-geo': 'br geo',
+  '/personal-website': 'personal website',
+  '/resume': 'resume',
+  '/split-it': 'split it!',
 }
 
 export { ptBR, enUS }
