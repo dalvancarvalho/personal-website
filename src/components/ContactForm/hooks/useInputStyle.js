815050aa658257e06194acb8bb9ddd2d237a6fab
Dalvan Carvalho <dalvan.bcarvalho@gmail.com>
2024-04-09 11:35:12 -0300

    renamed

---
+++ src/components/ContactForm/hooks/useInputStyle.js
@@ -0,0 +1,24 @@
+/* useInputStyle.js */
+
+import { useEffect, useRef } from 'react'
+
+function useInputStyle(inputName, inputs) {
+  // Handles the logic behind the input style
+
+  const container = useRef(null)
+  const input = useRef(null)
+
+  // Resets the input style after data submission
+  useEffect(() => checkInput(), [inputs[inputName]])
+
+  function checkInput() {
+    // Removes the 'highlighted' class if the input is empty and not focused
+
+    if (input.current.value.length === 0 && input.current !== document.activeElement)
+      container.current.classList.remove('highlighted')
+  }
+
+  return { checkInput, container, input }
+}
+
+export default useInputStyle
