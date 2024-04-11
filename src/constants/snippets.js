31057e5a131dcd9e53fdab9dcca7088a1303f945
Dalvan Carvalho <dalvan.bcarvalho@gmail.com>
2024-04-11 17:25:21 -0300

    update: snippet example

--- src/constants/snippets.js
+++ src/constants/snippets.js
@@ -14,7 +14,7 @@ import { useEffect, useState } from 'react'
 import Prism from 'prismjs'
 
 function useCodeSnippet(codeString) {
-  
+
   const [isCopied, setIsCopied] = useState(false)
 
   useEffect(() => {
