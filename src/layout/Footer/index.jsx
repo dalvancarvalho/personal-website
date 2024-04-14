7ecf654b7361fc58ddcee3bad2cc73e280ae1b36
Dalvan Carvalho <dalvan.bcarvalho@gmail.com>
2024-04-14 20:33:13 -0300

    removed: animation

--- src/layout/Footer/index.jsx
+++ src/layout/Footer/index.jsx
@@ -2,7 +2,7 @@
 
 import { useTranslation } from 'react-i18next'
 
-import useAnimate from './animations/useAnimate'
+// import useAnimate from './animations/useAnimate' | for now, the footer animation is disabled
 
 import Info from './components/Info'
 import Paragraph from '../../components/Text/Paragraph'
@@ -14,20 +14,17 @@ function Footer() {
   const year = new Date().getFullYear()
 
   const { t } = useTranslation()
-  const animation = useAnimate()
+  // const animation = useAnimate()
 
   return (
-    <footer
-      className="pt-4 pb-8 md:pb-16 text-slate-600 dark:text-gray-400 flex flex-col items-center justify-between gap-6 text-sm color-transition"
-      ref={animation.scope}
-    >
-      <div className="flex flex-col items-center gap-0.5" ref={animation.top}>
+    <footer className="pt-4 pb-8 md:pb-16 text-slate-600 dark:text-gray-400 flex flex-col items-center justify-between gap-6 text-sm color-transition">
+      <div className="flex flex-col items-center gap-0.5">
         <span className="mb-5 text-base">❖</span>
         <Info href="https://vercel.com" label="Vercel" paragraph={t('footer.deploy')} />
         <Info href="#about" label="Dalvan Carvalho" paragraph={t('footer.developer')} />
       </div>
-      <SocialNetworksList ref={animation.socials} t={t} />
-      <Paragraph ref={animation.bottom} variant="fixed-base-normal">
+      <SocialNetworksList t={t} />
+      <Paragraph variant="fixed-base-normal">
         &copy; {year} <b>dalvanc</b>.com
       </Paragraph>
     </footer>
