25990b5563220d79a2eedf39ba885574c09a010c
Dalvan Carvalho <dalvan.bcarvalho@gmail.com>
2024-04-15 12:21:20 -0300

    update: margin

--- src/pages/Resume/Download/index.jsx
+++ src/pages/Resume/Download/index.jsx
@@ -22,8 +22,8 @@ function Download(props) {
           title={t('pages.resume.download.heading')}
           watermark={t('pages.resume.download.watermark')}
         />
-        <Paragraph className="mb-16" i18nKey="pages.resume.download.paragraph" />
-        <div className="flex flex-wrap items-center justify-center gap-12">
+        <Paragraph i18nKey="pages.resume.download.paragraph" />
+        <div className="mt-12 md:mt-16 flex flex-wrap items-center justify-center gap-12">
           {resumeList.map((props) => (
             <ResumeCard key={props.title} t={t} {...props} />
           ))}
