9483fe8ba3128a596fedfc8a3d2993fba9ded3a4
Dalvan Carvalho <dalvan.bcarvalho@gmail.com>
2024-04-16 15:32:32 -0300

    update: example

--- src/constants/snippets.js
+++ src/constants/snippets.js
@@ -8,33 +8,33 @@ const snippetModel = {
   extension: 'js',                 // [string] displayed inside the code snippet box
   language: 'js',                  // [string] allowed languages: css, html, js
   codeString:                      // [string] shouldn't contain any spaces at the beginning
-  `/* useCodeSnippet.js */
+  `/* useCopyToClipboard.js */
 
 import { useEffect, useState } from 'react'
-import Prism from 'prismjs'
 
-function useCodeSnippet(codeString) {
+function useCopyToClipboard(string) {
+  // Copies the passed string to the clipboard
 
   const [isCopied, setIsCopied] = useState(false)
 
   useEffect(() => {
-    // Calls the Prism.js highlight functionality
+    if (!isCopied) return
 
-    if (typeof window !== 'undefined') Prism.highlightAll()
-  }, [])
+    const timeout = setTimeout(() => setIsCopied(false), 3000)
+
+    // Timeout cleanup
+    return () => clearTimeout(timeout)
+  }, [isCopied])
 
   function copyToClipboard() {
-    // Copies the snippet to the clipboard
-
-    navigator.clipboard.writeText(codeString)
+    navigator.clipboard.writeText(string)
     setIsCopied(true)
-    setTimeout(() => setIsCopied(false), 3000)
   }
 
   return { copyToClipboard, isCopied }
 }
 
-export default useCodeSnippet
+export default useCopyToClipboard
 `,
 }
 
