7e53efa76d7a4ae72ad7c1814836a02382e100e1
Dalvan Carvalho <dalvan.bcarvalho@gmail.com>
2024-04-16 15:31:02 -0300

    added: add to clipboard hook

---
+++ src/hooks/useCopyToClipboard.js
@@ -0,0 +1,27 @@
+/* useCopyToClipboard.js */
+
+import { useEffect, useState } from 'react'
+
+function useCopyToClipboard(string) {
+  // Copies the passed string to the clipboard
+
+  const [isCopied, setIsCopied] = useState(false)
+
+  useEffect(() => {
+    if (!isCopied) return
+
+    const timeout = setTimeout(() => setIsCopied(false), 3000)
+
+    // Timeout cleanup
+    return () => clearTimeout(timeout)
+  }, [isCopied])
+
+  function copyToClipboard() {
+    navigator.clipboard.writeText(string)
+    setIsCopied(true)
+  }
+
+  return { copyToClipboard, isCopied }
+}
+
+export default useCopyToClipboard
