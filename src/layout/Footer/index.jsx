562eba06e3c7ea366f099969d1376e4d73a0d5d5
Dalvan Carvalho <dalvan.bcarvalho@gmail.com>
2024-04-17 10:20:43 -0300

    removed: Info component

--- src/layout/Footer/index.jsx
+++ src/layout/Footer/index.jsx
@@ -2,9 +2,8 @@
 
 import { useTranslation } from 'react-i18next'
 
-// import useAnimate from './animations/useAnimate' | for now, the footer animation is disabled
+// import useAnimate from './animations/useAnimate' (for now, the footer animation is disabled)
 
-import Info from './components/Info'
 import Paragraph from '../../components/Text/Paragraph'
 import SocialNetworksList from './components/SocialNetworksList'
 
@@ -20,8 +19,28 @@ function Footer() {
     <footer className="pt-4 pb-8 md:pb-16 text-slate-600 dark:text-gray-400 flex flex-col items-center justify-between gap-6 text-sm color-transition">
       <div className="flex flex-col items-center gap-0.5">
         <span className="mb-5 text-base">❖</span>
-        <Info href="https://vercel.com" label="Vercel" paragraph={t('footer.deploy')} />
-        <Info href="#about" label="Dalvan Carvalho" paragraph={t('footer.developer')} />
+        <Paragraph variant="fixed-sm-normal">
+          {t('footer.deploy')}
+          <a
+            className="font-bold text-slate-600 dark:text-gray-400 focus-visible:text-slate-900 focus-visible:dark:text-gray-200 color-transition footer-link-hover"
+            href="https://vercel.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vercel
+          </a>
+        </Paragraph>
+        <Paragraph variant="fixed-sm-normal">
+          {t('footer.developer')}
+          <a
+            className="font-bold text-slate-600 dark:text-gray-400 focus-visible:text-slate-900 focus-visible:dark:text-gray-200 color-transition footer-link-hover"
+            href="https://dalvanc.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Dalvan Carvalho
+          </a>
+        </Paragraph>
       </div>
       <SocialNetworksList t={t} />
       <Paragraph variant="fixed-base-normal">
