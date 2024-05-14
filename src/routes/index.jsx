65939e034eefb5da12fa1f507c5302ddc976b2dc
Dalvan Carvalho <dalvan.bcarvalho@gmail.com>
2024-05-14 15:54:12 -0300

    refactor: elements alignment

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -13,19 +13,20 @@ const PersonalWebsite = lazy(() => import('../pages/PersonalWebsite'))
 const Resume = lazy(() => import('../pages/Resume'))
 const SplitIt = lazy(() => import('../pages/SplitIt'))
 
+// prettier-ignore
 function AppRoutes() {
   // Manages the page rendered to each specified route
   // Note: If a new route is added, update the file 'logoText.js'
 
   return (
     <Routes>
-      <Route path="/" element={<Fallback page={<Home />} />} />
-      <Route path="/404" element={<Fallback page={<NotFound />} />} />
-      <Route path="/br-geo" element={<Fallback page={<BRGeo />} />} />
+      <Route path="/"                 element={<Fallback page={<Home />} />} />
+      <Route path="/404"              element={<Fallback page={<NotFound />} />} />
+      <Route path="/br-geo"           element={<Fallback page={<BRGeo />} />} />
       <Route path="/personal-website" element={<Fallback page={<PersonalWebsite />} />} />
-      <Route path="/resume" element={<Fallback page={<Resume />} />} />
-      <Route path="/split-it" element={<Fallback page={<SplitIt />} />} />
-      <Route path="*" element={<Navigate to="/404" />} />
+      <Route path="/resume"           element={<Fallback page={<Resume />} />} />
+      <Route path="/split-it"         element={<Fallback page={<SplitIt />} />} />
+      <Route path="*"                 element={<Navigate to="/404" />} />
     </Routes>
   )
 }
