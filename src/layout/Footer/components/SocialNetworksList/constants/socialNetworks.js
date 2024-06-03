b2f23c2cf1180b1b8d3c65f782b988fb843a8cd3
Dalvan Carvalho <dalvan.bcarvalho@gmail.com>
2024-06-02 21:30:25 -0300

    added: steam link

--- src/layout/Footer/components/SocialNetworksList/constants/socialNetworks.js
+++ src/layout/Footer/components/SocialNetworksList/constants/socialNetworks.js
@@ -5,6 +5,7 @@ import {
   faLastfm,
   faLinkedin,
   faSpotify,
+  faSteam,
   faXTwitter,
 } from '@fortawesome/free-brands-svg-icons'
 
@@ -12,34 +13,46 @@ const SOCIAL_NETWORKS = [
   // List of social networks displayed in the footer of each page of the application
 
   {
+    id: crypto.randomUUID(),
     name: 'GitHub',
     icon: faGithub,
     url: 'https://github.com/dalvancarvalho',
     hoverColor: 'hover:text-[#14191e] focus-visible:text-[#14191e]',
   },
   {
+    id: crypto.randomUUID(),
     name: 'LinkedIn',
     icon: faLinkedin,
     url: 'https://www.linkedin.com/in/dalvancarvalho',
     hoverColor: 'hover:text-[#0a66c2] focus-visible:text-[#0a66c2]',
   },
   {
+    id: crypto.randomUUID(),
     name: 'Twitter',
     icon: faXTwitter,
-    url: 'https://twitter.com/theRealZackyV',
+    url: 'https://x.com/theRealZackyV',
     hoverColor: 'hover:text-[#0f1419] focus-visible:text-[#0f1419]',
   },
   {
-    name: 'Last.fm',
-    icon: faLastfm,
-    url: 'https://www.last.fm/user/dalvancarvalho',
-    hoverColor: 'hover:text-[#d0232b] focus-visible:text-[#d0232b]',
+    id: crypto.randomUUID(),
+    name: 'Steam',
+    icon: faSteam,
+    url: 'https://steamcommunity.com/id/dalvancarvalho',
+    hoverColor: 'hover:text-[#235684] focus-visible:text-[#235684]',
   },
   {
+    id: crypto.randomUUID(),
     name: 'Spotify',
     icon: faSpotify,
     url: 'https://open.spotify.com/user/dalvancarvalho',
     hoverColor: 'hover:text-[#25d865] focus-visible:text-[#25d865]',
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'Last.fm',
+    icon: faLastfm,
+    url: 'https://www.last.fm/user/dalvancarvalho',
+    hoverColor: 'hover:text-[#d0232b] focus-visible:text-[#d0232b]',
   },
 ]
 
