b4707216dc86c61bfd4d1bd6efd93b2d3d3b6188
Dalvan Carvalho <dalvan.bcarvalho@gmail.com>
2024-06-14 15:38:33 -0300

    update: image instead text

--- src/pages/Home/Projects/components/Card/components/Tech/index.jsx
+++ src/pages/Home/Projects/components/Card/components/Tech/index.jsx
@@ -1,18 +1,20 @@
 /* Tech/index.jsx */
 
-export default function Tech({ isEven, tech, url }) {
+export default function Tech({ image, tech, url }) {
   // Link to the tech's page
 
   return (
-    <li className={isEven ? 'translate-x-[200%]' : '-translate-x-[200%]'}>
-      <a
-        className="text-sm md:text-base text-slate-50 font-semibold card-tech-hover"
-        href={url}
-        rel="noopener noreferrer"
-        tabIndex="-1"
-        target="_blank"
-      >
-        {tech}
+    <li
+      className="mx-auto group/tech
+      group-data-[parity=even]/card:translate-x-[200%]
+      group-data-[parity=odd]/card:-translate-x-[200%]"
+    >
+      <a href={url} rel="noopener noreferrer" tabIndex="-1" target="_blank" title={tech}>
+        <img
+          className="size-5 md:size-6 brightness-0 invert md:group-hover/tech:scale-110 select-none transition-transform duration-150"
+          src={image.src}
+          alt={image.alt}
+        />
       </a>
     </li>
   )
