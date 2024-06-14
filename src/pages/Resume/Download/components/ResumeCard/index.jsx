6a2956663c8d4cd1a292b06d9d5037e2945b4e76
Dalvan Carvalho <dalvan.bcarvalho@gmail.com>
2024-06-14 15:37:39 -0300

    update: blur on hover

--- src/pages/Resume/Download/components/ResumeCard/index.jsx
+++ src/pages/Resume/Download/components/ResumeCard/index.jsx
@@ -1,7 +1,7 @@
 /* ResumeCard/index.jsx */
 
 import { useState } from 'react'
-import { faArrowDown } from '@fortawesome/free-solid-svg-icons'
+import { faArrowDown, faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import useTheme from '../../../../../context/ThemeContext'
@@ -13,7 +13,7 @@ import FormatSelector from './components/FormatSelector'
 import resume from '../../../../../assets/svg/resume.svg'
 import resumeDark from '../../../../../assets/svg/resume-dark.svg'
 
-export default function ResumeCard({ href, thumbnail, t, title }) {
+export default function ResumeCard({ href, t, title }) {
   // Downloadable resume
 
   const [format, setFormat] = useState('pdf')
@@ -22,31 +22,31 @@ export default function ResumeCard({ href, thumbnail, t, title }) {
   return (
     <div className="max-w-max flex flex-col items-start group/card">
       <a
-        className="relative mb-3 md:mb-4 rounded-md border-l-[5px] border-accent color-transition overflow-hidden group/image"
+        className="relative mb-3 md:mb-4 rounded-md border-l-[5px] border-accent color-transition overflow-hidden group/link"
         href={href + format}
         rel="noopener noreferrer"
         target="_blank"
       >
         <img
           alt={t('pages.resume.download.altText')}
-          className="w-[295px] md:w-80 h-auto select-none"
+          className="w-[295px] md:w-80 h-auto group-hover/link:blur-[2px] group-focus-visible/link:blur-[2px] group-hover/link:dark:brightness-50 group-focus-visible/link:dark:brightness-50 select-none transition-[filter] duration-300"
           src={theme === 'light' ? resume : resumeDark}
         />
 
-        {/* Overlay displayed when the card is hovered in medium/large screens */}
+        {/* Overlay displayed when the card is hovered on medium/large screens */}
         <div className="invisible md:visible">
-          <div className="absolute top-0 left-0 h-full w-1/4 -translate-x-[110%] group-hover/image:translate-x-0 group-focus-visible/image:translate-x-0 flex items-center justify-center bg-gradient-to-r from-blue-600 to-blue-500 dark:from-orange-500 dark:to-orange-500 !transition-all !duration-300">
+          <div className="absolute inset-0 h-full w-1/4 -translate-x-full group-hover/link:translate-x-0 group-focus-visible/link:translate-x-0 grid place-items-center bg-accent !transition-all !duration-300">
             <FontAwesomeIcon
-              className="-translate-x-[2px] rotate-[225deg] text-white"
-              icon={faArrowDown}
+              className="-translate-x-[2px] text-white"
+              icon={faArrowUpRightFromSquare}
               size="xl"
             />
           </div>
-          <div className="absolute top-0 right-0 w-3/4 h-full flex items-center justify-center backdrop-blur-sm dark:backdrop-brightness-[50%] opacity-0 group-hover/image:opacity-100 group-focus-visible/image:opacity-100 transition-all duration-300 delay-75">
+          <div className="absolute top-0 right-0 translate-x-12 group-hover/link:translate-x-0 group-focus-visible/link:translate-x-0 w-3/4 h-full grid place-items-center opacity-0 group-hover/link:opacity-100 group-focus-visible/link:opacity-100 transition-all duration-300">
             <Paragraph
               className="!text-black dark:!text-white"
               i18nKey="pages.resume.download.newTab"
-              variant="fixed-lg-semibold"
+              variant="fixed-lg-bold"
             />
           </div>
         </div>
