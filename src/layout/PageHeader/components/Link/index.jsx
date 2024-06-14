4fb36a36c46a1a58b652ce8f097985f07c7617f3
Dalvan Carvalho <dalvan.bcarvalho@gmail.com>
2024-06-14 15:36:48 -0300

    update: external link icon

--- src/layout/PageHeader/components/Link/index.jsx
+++ src/layout/PageHeader/components/Link/index.jsx
@@ -1,6 +1,6 @@
 /* Link/index.jsx */
 
-import { faArrowUp } from '@fortawesome/free-solid-svg-icons'
+import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default function Link({ icon, href, label }) {
@@ -8,7 +8,7 @@ export default function Link({ icon, href, label }) {
 
   return (
     <a
-      className="max-w-max px-4 py-2 flex items-center gap-3 bg-slate-250 dark:bg-dark-2 text-slate-600 dark:text-gray-400 focus-visible:text-slate-800 focus-visible:dark:text-gray-200 rounded-full link-hover color-transition"
+      className="max-w-max px-4 py-2 flex items-center gap-2 bg-slate-250 dark:bg-dark-2 text-slate-600 dark:text-gray-400 focus-visible:text-slate-800 focus-visible:dark:text-gray-200 rounded-full link-hover color-transition"
       href={href}
       rel="noopener noreferrer"
       target="_blank"
@@ -18,7 +18,7 @@ export default function Link({ icon, href, label }) {
         icon={icon}
       />
       {label}
-      <FontAwesomeIcon className="rotate-45" icon={faArrowUp} />
+      <FontAwesomeIcon size="sm" icon={faArrowUpRightFromSquare} />
     </a>
   )
 }
