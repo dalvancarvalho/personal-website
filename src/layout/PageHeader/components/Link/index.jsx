99a0551203845380551fb446c1fa5dd0b4c8d428
Dalvan Carvalho <dalvan.bcarvalho@gmail.com>
2024-06-17 11:02:19 -0300

    update: colors

--- src/layout/PageHeader/components/Link/index.jsx
+++ src/layout/PageHeader/components/Link/index.jsx
@@ -8,17 +8,11 @@ export default function Link({ icon, href, label }) {
 
   return (
     <a
-      className="max-w-max h-10 rounded-full pl-3 pr-4 flex items-center gap-2 bg-blue-400/25 dark:bg-orange-500/25 text-slate-600 dark:text-gray-400 color-transition group/link
-      md:bg-slate-250
-      md:dark:bg-dark-2
-      hover:bg-blue-300/50
-      focus-visible:bg-blue-400/25
-      hover:dark:bg-orange-500/25
-      focus-visible:dark:bg-orange-500/25
-      hover:text-slate-800
-      focus-visible:text-slate-800
-      hover:dark:text-gray-200
-      focus-visible:dark:text-gray-200"
+      className="max-w-max h-10 rounded-full pl-3 pr-4 flex items-center gap-2 bg-slate-250 dark:bg-dark-2 text-slate-600 dark:text-gray-400 color-transition
+      hover:bg-blue-400/25        focus-visible:bg-blue-400/25
+      hover:dark:bg-orange-500/25 focus-visible:dark:bg-orange-500/25
+      hover:text-slate-800        focus-visible:text-slate-800
+      hover:dark:text-gray-200    focus-visible:dark:text-gray-200"
       href={href}
       rel="noopener noreferrer"
       target="_blank"
