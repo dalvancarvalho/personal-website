193855bf8cde094107f9bb722481cef5b65282b6
Dalvan Carvalho <dalvan.bcarvalho@gmail.com>
2024-06-17 13:42:24 -0300

    update: utility class tokens

--- src/layout/PageHeader/components/Link/index.jsx
+++ src/layout/PageHeader/components/Link/index.jsx
@@ -4,15 +4,19 @@ import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default function Link({ icon, href, label }) {
-  // External access link used inside the PageHeader component
+  // External access link used inside the 'PageHeader' component
 
   return (
     <a
       className="max-w-max h-10 rounded-full pl-3 pr-4 flex items-center gap-2 bg-slate-250 dark:bg-dark-2 text-slate-600 dark:text-gray-400 color-transition
-      hover:bg-blue-400/25        focus-visible:bg-blue-400/25
-      hover:dark:bg-orange-500/25 focus-visible:dark:bg-orange-500/25
-      hover:text-slate-800        focus-visible:text-slate-800
-      hover:dark:text-gray-200    focus-visible:dark:text-gray-200"
+      focus-visible:bg-blue-400/25
+              hover:bg-blue-400/25        
+      focus-visible:dark:bg-orange-500/25
+              hover:dark:bg-orange-500/25
+      focus-visible:text-slate-800
+              hover:text-slate-800       
+      focus-visible:dark:text-gray-200
+              hover:dark:text-gray-200"
       href={href}
       rel="noopener noreferrer"
       target="_blank"
