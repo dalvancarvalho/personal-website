6e7adf6724daecfc6ece7db9eeb21190d722a6c7
Dalvan Carvalho <dalvan.bcarvalho@gmail.com>
2024-06-21 17:41:33 -0300

    bug fix: useEffect dependency array

--- src/utils/ScreenPanel/hooks/useScreenPanel.js
+++ src/utils/ScreenPanel/hooks/useScreenPanel.js
@@ -5,18 +5,16 @@ import { useEffect, useState } from 'react'
 import useLocalStorage from '../../../hooks/useLocalStorage'
 import useScreenProps from '../../../hooks/useScreenProps'
 
+// prettier-ignore
 export default function useScreenPanel() {
   // Defines the properties displayed in the screen panel
 
-  const [isPanelExpanded, setPanelState] = useLocalStorage(
-    '_dev_expanded-screen-panel',
-    false
-  )
+  const [screen, setScreen] = useState('...')
+  const [isPanelExpanded, setPanelExpanded] = useLocalStorage('expanded-screen-panel', false)
   const { isPortraitMode, screenDimensions, screenSize } = useScreenProps()
-  const [screen, setScreenSize] = useState(null)
+  const { width, height } = screenDimensions
 
   const orientation = isPortraitMode ? 'Portrait' : 'Landscape'
-  const { width, height } = screenDimensions
   const viewportSize = width + ' x ' + height
 
   useEffect(() => {
@@ -26,28 +24,28 @@ export default function useScreenPanel() {
     switch (position) {
       case -1:
       case 0:
-        setScreenSize('sm')
+        setScreen('sm')  // 640px
         break
       case 1:
-        setScreenSize('md')
+        setScreen('md')  // 768px
         break
       case 2:
-        setScreenSize('lg')
+        setScreen('lg')  // 1024px
         break
       case 3:
-        setScreenSize('xl')
+        setScreen('xl')  // 1280px
         break
       case 4:
-        setScreenSize('2xl')
+        setScreen('2xl') // 1536px
         break
       default:
-        setScreenSize('...')
+        setScreen('...')
     }
-  }, [width])
+  }, [screenSize])
 
   function togglePanel(event) {
     event.stopPropagation()
-    setPanelState((current) => !current)
+    setPanelExpanded((current) => !current)
   }
 
   return { isPanelExpanded, togglePanel, orientation, viewportSize, screen }
