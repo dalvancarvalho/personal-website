d359ccbc827cec4e537a9136a59333f538ce4e3a
Dalvan Carvalho <dalvan.bcarvalho@gmail.com>
2024-06-21 17:40:48 -0300

    refactor: spacing

--- src/utils/ScreenPanel/components/TableCells/index.jsx
+++ src/utils/ScreenPanel/components/TableCells/index.jsx
@@ -3,9 +3,11 @@
 export function TableHeader({ children, ...props }) {
   return <th {...props}>{children}</th>
 }
+
 export function TableRow({ children, ...props }) {
   return <tr {...props}>{children}</tr>
 }
+
 export function TableData({ children, ...props }) {
   return <td {...props}>{children}</td>
 }
