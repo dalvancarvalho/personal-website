38b1935eecfc0cae6237406749700382b1b8f190
Dalvan Carvalho <dalvan.bcarvalho@gmail.com>
2024-07-02 13:16:53 -0300

    update: margin

--- src/components/Toast/index.jsx
+++ src/components/Toast/index.jsx
@@ -10,12 +10,12 @@ export function Toast({ id, variant }) {
   // Displays simple messages in the bottom of the screen
 
   const { t } = useTranslation()
-  const { deleteToast, toastProps } = useToast(variant)
+  const { removeToast, toastProps } = useToast(variant)
   const { heading, description, icon } = toastProps
 
   return (
     <div className="relative xs:w-96 rounded-lg border border-slate-150 dark:border-dark-1 shadow-md p-4 sm:p-3.5 bg-white dark:bg-dark-2 flex items-center gap-2.5 font-urbanist !transition-[background-color,border] !duration-300">
-      <FontAwesomeIcon className={`!size-5 ${icon.color}`} icon={icon.svg} />
+      <FontAwesomeIcon className={`sm:!ml-px !size-5 ${icon.color}`} icon={icon.svg} />
       <div className="text-sm">
         <h3 className="font-bold">{t(heading)}</h3>
         <p className="text-slate-600 dark:text-gray-400 color-transition">
@@ -28,7 +28,7 @@ export function Toast({ id, variant }) {
         hover:dark:text-gray-200
         focus-visible:text-slate-900
         focus-visible:dark:text-gray-200"
-        onClick={() => deleteToast(id)}
+        onClick={() => removeToast(id)}
       >
         <FontAwesomeIcon icon={faClose} />
         <p className="sr-only">{t('toast.close')}</p>
