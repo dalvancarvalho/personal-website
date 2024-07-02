b14b3ebdf867bfd5f43cc370153598e51ae039c8
Dalvan Carvalho <dalvan.bcarvalho@gmail.com>
2024-07-02 12:55:22 -0300

    added: useToast

---
+++ src/components/Toast/hooks/useToast.jsx
@@ -0,0 +1,23 @@
+/* useToast.jsx */
+
+import { toast } from 'sonner'
+
+import { Toast } from '..'
+
+import TOAST_VARIANTS from '../constants/toastVariants'
+
+export function useToast(variant) {
+  // Handles the logic of the 'Toast' component
+
+  const toastProps = TOAST_VARIANTS[variant]
+
+  function createToast(variant) {
+    toast.custom((id) => <Toast id={id} variant={variant} />)
+  }
+
+  function deleteToast(id) {
+    toast.dismiss(id)
+  }
+
+  return { createToast, deleteToast, toastProps }
+}
--- src/components/Toast/index.jsx
+++ src/components/Toast/index.jsx
@@ -2,21 +2,20 @@
 
 import { faClose } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { toast } from 'sonner'
 import { useTranslation } from 'react-i18next'
 
-import TOAST_VARIANTS from './constants/toastVariants'
+import { useToast } from './hooks/useToast'
 
-export default function Toast({ id, variant }) {
+export function Toast({ id, variant }) {
   // Displays simple messages in the bottom of the screen
 
-  const { heading, description, icon } = TOAST_VARIANTS[variant]
-
   const { t } = useTranslation()
+  const { deleteToast, toastProps } = useToast(variant)
+  const { heading, description, icon } = toastProps
 
   return (
-    <div className="relative xs:w-96 rounded-lg border border-slate-150 dark:border-dark-1 shadow-md p-4 sm:p-3.5 bg-white dark:bg-dark-2 flex items-center gap-2 font-urbanist !transition-[background-color,border] !duration-300">
-      <FontAwesomeIcon className={icon.color} icon={icon.svg} />
+    <div className="relative xs:w-96 rounded-lg border border-slate-150 dark:border-dark-1 shadow-md p-4 sm:p-3.5 bg-white dark:bg-dark-2 flex items-center gap-2.5 font-urbanist !transition-[background-color,border] !duration-300">
+      <FontAwesomeIcon className={`!size-5 ${icon.color}`} icon={icon.svg} />
       <div className="text-sm">
         <h3 className="font-bold">{t(heading)}</h3>
         <p className="text-slate-600 dark:text-gray-400 color-transition">
@@ -24,12 +23,12 @@ export default function Toast({ id, variant }) {
         </p>
       </div>
       <button
-        className="hidden sm:block absolute top-2 right-1 text-slate-400 dark:text-gray-500 color-transition
+        className="absolute top-2 right-1 text-slate-400 dark:text-gray-500 color-transition
         hover:text-slate-900
         hover:dark:text-gray-200
         focus-visible:text-slate-900
         focus-visible:dark:text-gray-200"
-        onClick={() => toast.dismiss(id)}
+        onClick={() => deleteToast(id)}
       >
         <FontAwesomeIcon icon={faClose} />
         <p className="sr-only">{t('toast.close')}</p>
