94b64240833a78f95f617973193c96ee8e84534f
Dalvan Carvalho <dalvan.bcarvalho@gmail.com>
2024-07-02 12:55:37 -0300

    removed: toaster

--- src/pages/Home/Contact/index.jsx
+++ src/pages/Home/Contact/index.jsx
@@ -1,7 +1,5 @@
 /* Contact/index.jsx */
 
-import { Toaster } from 'sonner'
-
 import useConfetti from '../../../context/ConfettiContext'
 import useAnimate from './animations/useAnimate'
 
@@ -25,40 +23,37 @@ export default function Contact({ screenSize, t }) {
   const animation = useAnimate()
 
   return (
-    <>
-      <Section className="relative" id="contact" ref={animation.scope}>
-        <Container>
-          <Heading
-            heading="pages.home.contact.heading"
-            sectionName="pages.home.contact.sectionName"
-          />
-          <Grid className="gap-x-12">
-            <div className="h-full col-start-1 col-end-8 flex flex-col justify-between gap-y-12">
-              <div className="flex flex-col gap-4">
-                <Paragraph
-                  className="contact-paragraph"
-                  i18nKey="pages.home.contact.paragraphI"
-                />
-                <Paragraph
-                  className="contact-paragraph"
-                  i18nKey="pages.home.contact.paragraphII"
-                />
-              </div>
-              <div className="w-max flex flex-col gap-2.5" ref={animation.contacts}>
-                {CONTACTS.map(({ id, ...props }) => (
-                  <ContactInfo key={id} t={t} {...props} />
-                ))}
-              </div>
+    <Section className="relative" id="contact" ref={animation.scope}>
+      <Container>
+        <Heading
+          heading="pages.home.contact.heading"
+          sectionName="pages.home.contact.sectionName"
+        />
+        <Grid className="gap-x-12">
+          <div className="h-full col-start-1 col-end-8 flex flex-col justify-between gap-y-12">
+            <div className="flex flex-col gap-4">
+              <Paragraph
+                className="contact-paragraph"
+                i18nKey="pages.home.contact.paragraphI"
+              />
+              <Paragraph
+                className="contact-paragraph"
+                i18nKey="pages.home.contact.paragraphII"
+              />
             </div>
-            {!screenSize.lg && <Divider />}
-            <div className="col-start-8 col-end-13 w-full">
-              <ContactForm />
+            <div className="w-max flex flex-col gap-2.5" ref={animation.contacts}>
+              {CONTACTS.map(({ id, ...props }) => (
+                <ContactInfo key={id} t={t} {...props} />
+              ))}
             </div>
-          </Grid>
-        </Container>
-        {confetti && <Confetti />}
-      </Section>
-      <Toaster position="bottom-center" />
-    </>
+          </div>
+          {!screenSize.lg && <Divider />}
+          <div className="col-start-8 col-end-13 w-full">
+            <ContactForm />
+          </div>
+        </Grid>
+      </Container>
+      {confetti && <Confetti />}
+    </Section>
   )
 }
