3148e33fab2ffd4eb8e85658aef20b88131b0e46
Dalvan Carvalho <dalvan.bcarvalho@gmail.com>
2024-07-03 14:33:26 -0300

    update: dark mode color

--- src/components/Toast/index.jsx
+++ src/components/Toast/index.jsx
@@ -14,20 +14,22 @@ export function Toast({ id, variant }) {
   const { heading, description, icon } = toastProps
 
   return (
-    <div className="relative xs:w-96 rounded-lg border border-slate-150 dark:border-dark-1 shadow-md p-4 sm:p-3.5 bg-white dark:bg-dark-2 flex items-center gap-2.5 font-urbanist !transition-[background-color,border] !duration-300">
+    <div className="relative xs:w-96 sm:h-[72px] rounded-lg border border-slate-150 dark:border-neutral-600 shadow-md p-4 sm:p-3.5 bg-white dark:bg-neutral-700 flex items-center gap-2.5 font-urbanist !transition-[background-color,border] !duration-300">
       <FontAwesomeIcon className={`sm:!ml-px !size-5 ${icon.color}`} icon={icon.svg} />
       <div className="text-sm">
-        <h3 className="font-bold">{t(heading)}</h3>
-        <p className="text-slate-600 dark:text-gray-400 color-transition">
+        <h3 className="font-bold text-slate-800 dark:text-white color-transition">
+          {t(heading)}
+        </h3>
+        <p className="text-slate-600 dark:text-gray-200 color-transition">
           {t(description)}
         </p>
       </div>
       <button
-        className="absolute top-2 right-1 text-slate-400 dark:text-gray-500 color-transition
+        className="absolute top-2 right-1 text-slate-400 dark:text-gray-400 color-transition
         hover:text-slate-900
-        hover:dark:text-gray-200
+        hover:dark:text-white
         focus-visible:text-slate-900
-        focus-visible:dark:text-gray-200"
+        focus-visible:dark:text-white"
         onClick={() => removeToast(id)}
       >
         <FontAwesomeIcon icon={faClose} />
